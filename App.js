b3991a797cd0d87c53321db43ee1a717aad040b3
Ahmed <ahmed.e.alfarra@gmail.com>
2022-06-09 00:19:11 +0300

    Location Permission

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+
+const WEATHER_API_KEY = "9033e10f66d05fc0c557ccbea72fba1b";
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app.");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      alert(`Latitude : ${latitude}
+      Longitude : ${longitude}`);
+    } catch (error) {}
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
